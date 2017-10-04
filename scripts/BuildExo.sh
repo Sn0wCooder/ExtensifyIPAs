4e203a7298d0be3382cdac1bf02131fb0234c527
Sn0wCooder <leonardo@iMac-di-Leonardo.local>
2017-10-04 21:20:36 +0200

    Updated script: now it supports creation of multiple Exos

--- scripts/BuildExo.sh
+++ scripts/BuildExo.sh
@@ -27,31 +27,113 @@ filename="${filename%.*}"
 
 echo "IPA selected for build Exo: $filename"
 
-echo "Downloading Exo... (it will take a while!)"
+echo "Downloading Exo(s)... (it will take a while!)"
 
-tweakv=$(svn ls https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename) > /dev/null 2>&1
+#tweakv=$(svn ls https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename) > /dev/null 2>&1
 
-svn checkout https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename/$tweakv > /dev/null 2>&1
-
-tweakv=${tweakv%/}
-
-cp -r $tweakv/* Payload/$filename.app/
-
+#echo $tweakv >> tweakv.txt
+# 
+# 
+# 
+svn ls https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename >> ../tweakv.txt
 EXT=dylib
-for i in $tweakv/*; do
-	if [ "${i}" != "${i%.${EXT}}" ];then
-		dylibname=$(basename $i)
-	    echo "Injecting $dylibname..."
-		optool install -c load -p @executable_path/$dylibname -t Payload/*/$filename > /dev/null 2>&1
-	fi
+cat ../tweakv.txt | while read p
+do
+	
+	rm -rf *
+	
+	svn checkout https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename/$p > /dev/null 2>&1
+	
+	ditto -xk $crackedipa . > /dev/null 2>&1
+	
+	p=${p%/}
+	
+	echo $p >> ../twk.txt
+	
+	echo "I'm building Exo $p..."
+
+	cp -r $p/* Payload/$filename.app/
+
+	for i in $p/*; do
+		if [ "${i}" != "${i%.${EXT}}" ];then
+			dylibname=$(basename $i)
+			echo "Injecting $dylibname..."
+			optool install -c load -p @executable_path/$dylibname -t Payload/*/$filename > /dev/null 2>&1
+		fi
+	done
+	
+	echo "Repacking..."
+	zip -9qry "app.ipa" "Payload" > /dev/null 2>&1
+	BUNDLE_VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" Payload/*/Info.plist)
+
+	newfilename=$filename-v$BUNDLE_VERSION-$p.ipa
+	newfilename=`echo $newfilename|tr '-' '_'`
+
+	mv app.ipa ~/Desktop/$newfilename
+	
+	#rm -rf app.ipa
+	
+	echo "Saved with filename $newfilename"
+
 done
-echo "Repacking..."
-zip -9qry "app.ipa" "Payload" > /dev/null 2>&1
-BUNDLE_VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" Payload/*/Info.plist)
 
-newfilename=$filename-v$BUNDLE_VERSION-$tweakv.ipa
-newfilename=`echo $newfilename|tr '-' '_'`
 
-mv app.ipa ~/Desktop/$newfilename
 
-echo "Saved with filename $newfilename"
+# if number of ipas > 1
+echo
+echo
+echo
+
+rm -rf *
+cat ../tweakv.txt | while read p
+do
+	svn checkout https://github.com/Sn0wCooder/Extensify-Exos/trunk/$filename/$p > /dev/null 2>&1
+done
+
+NUMBEROFTWEAKS=$(find ./* -maxdepth 0 -type d | wc -l)
+
+echo "Number of tweaks: $NUMBEROFTWEAKS"
+
+if (( NUMBEROFTWEAKS > 1 )); then
+	
+
+
+	ditto -xk $crackedipa . > /dev/null 2>&1
+
+	cat ../twk.txt | while read p
+	do
+		echo -n "_$p" >> ../tweakadded.txt
+		cp -r $p/* Payload/$filename.app
+		
+		for i in $p/*; do
+			if [ "${i}" != "${i%.${EXT}}" ];then
+				dylibname=$(basename $i)
+				echo "Injecting $dylibname..."
+				optool install -c load -p @executable_path/$dylibname -t Payload/*/$filename > /dev/null 2>&1
+			fi
+		done
+	done
+
+	echo "Repacking..."
+	zip -9qry "app.ipa" "Payload" > /dev/null 2>&1
+	BUNDLE_VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" Payload/*/Info.plist)
+
+#cat ../twk.txt | while read p
+#do
+#	echo "_$p" > ../tweakadded.txt
+#done
+
+	tweakadded=$(cat ../tweakadded.txt)
+
+	newfilename=$filename-v$BUNDLE_VERSION$tweakadded.ipa
+	newfilename=`echo $newfilename|tr '-' '_'`
+
+	mv app.ipa ~/Desktop/$newfilename
+
+fi
+
+rm -rf ../tweakv.txt > /dev/null 2>&1
+rm -rf ../twk.txt > /dev/null 2>&1
+rm -rf ../tweakadded.txt > /dev/null 2>&1
+
+echo "FINISHED!"
